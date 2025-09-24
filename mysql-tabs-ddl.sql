3942762de5afb9935bd334d4cedb13aff4abc42b
SongHeQing <2063298305@qq.com>
2025-09-24 19:59:46 +0800

    新增了个人主页相关代码

--- mysql-tabs-ddl.sql
+++ mysql-tabs-ddl.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `article` (
+REATE TABLE `article` (
   `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID,主键',
   `user_id` int unsigned NOT NULL COMMENT '用户ID',
   `channel_id` int unsigned NOT NULL COMMENT '频道ID',
@@ -16,8 +16,9 @@ CREATE TABLE `article` (
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   PRIMARY KEY (`id`),
   KEY `idx_article_create_time` (`create_time`),
-  KEY `channel_id` (`channel_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='文章表'
+  KEY `channel_id` (`channel_id`),
+  KEY `article_user_id_index` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='文章表'
 
 CREATE TABLE `article_collection_previews` (
   `id` int unsigned NOT NULL COMMENT '文章ID (与article.id相同，作为预览记录的主键)',
@@ -40,12 +41,13 @@ CREATE TABLE `article_likes` (
   UNIQUE KEY `UQ_user_article_like` (`user_id`,`article_id`),
   KEY `IX_article_likes_user_id` (`user_id`,`create_time`),
   KEY `IX_article_likes_article_id` (`article_id`,`create_time`)
-) ENGINE=InnoDB AUTO_INCREMENT=129 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='文章点赞记录表'
+) ENGINE=InnoDB AUTO_INCREMENT=172 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='文章点赞记录表'
 
 CREATE TABLE `channel` (
   `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID,主键',
   `channelname` varchar(96) NOT NULL COMMENT '频道名',
   `image` varchar(255) DEFAULT NULL COMMENT '频道封面',
+  `background` varchar(255) DEFAULT NULL COMMENT '频道背景',
   `user_count` int unsigned DEFAULT '0' COMMENT '用户数量',
   `article_count` int unsigned DEFAULT '0' COMMENT '文章数量',
   `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
@@ -72,13 +74,15 @@ CREATE TABLE `comment` (
   `user_id` int unsigned NOT NULL COMMENT '评论所属用户 ID',
   `reply_count` int unsigned DEFAULT '0' COMMENT '二级评论数',
   `reply_to_user_id` int unsigned DEFAULT NULL COMMENT '回复目标用户 ID（用于 UI 展示）',
+  `like_count` int unsigned DEFAULT '0' COMMENT '点赞数',
   `content` varchar(2000) NOT NULL COMMENT '评论内容',
   `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`),
   KEY `idx_article_id_parent_id_create_time` (`article_id`,`parent_id`,`create_time`),
-  KEY `idx_parent_id_create_time` (`parent_id`,`create_time`)
-) ENGINE=InnoDB AUTO_INCREMENT=74 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+  KEY `idx_parent_id_create_time` (`parent_id`,`create_time`),
+  KEY `comment_user_id_index` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=151 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='评论表'
 
 CREATE TABLE `comment_likes` (
   `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT '评论点赞记录ID',
@@ -90,7 +94,7 @@ CREATE TABLE `comment_likes` (
   UNIQUE KEY `UQ_user_comment_like` (`user_id`,`comment_id`),
   KEY `IX_comment_likes_user_id` (`user_id`,`create_time`),
   KEY `IX_comment_likes_comment_id` (`comment_id`,`create_time`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='评论点赞记录表'
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='评论点赞记录表'
 
 CREATE TABLE `images` (
   `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT '图片ID,主键',
@@ -111,14 +115,20 @@ CREATE TABLE `user` (
   `password` varchar(16) NOT NULL COMMENT '密码',
   `nickname` varchar(20) NOT NULL COMMENT '昵称',
   `image` varchar(255) DEFAULT NULL COMMENT '头像',
-  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
-  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+  `article_send_count` int unsigned NOT NULL DEFAULT '0' COMMENT '文章发送量',
+  `comment_send_count` int unsigned NOT NULL DEFAULT '0' COMMENT '评论发送量',
+  `channel_follow_count` int unsigned NOT NULL DEFAULT '0' COMMENT '频道关注量',
+  `follow_count` int unsigned NOT NULL DEFAULT '0' COMMENT '用户关注量',
+  `fans_count` int unsigned NOT NULL DEFAULT '0' COMMENT '粉丝数量',
+  `like_count` int unsigned NOT NULL DEFAULT '0' COMMENT '获赞量',
+  `introduction` varchar(500) DEFAULT NULL COMMENT '个人简介',
+  `create_time` datetime DEFAULT (now()) COMMENT '创建时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `username` (`nickname`),
   UNIQUE KEY `phone` (`phone`),
   KEY `user_email_password_index` (`email`,`password`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户表'
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户表'
 
 CREATE TABLE `user_collections` (
   `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT '收藏关系ID',
@@ -141,4 +151,3 @@ CREATE TABLE `user_follows` (
   KEY `IX_user_follows_follower_id` (`follower_id`,`create_time`),
   KEY `IX_user_follows_following_id` (`following_id`,`create_time`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户关注关系表'
-
